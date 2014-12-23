782fc899791e2e3fca3e00814dabc5a8f5c0060a
Michael <michael@mail.example.com>
2014-12-23 18:25:39 -0500

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,185 @@
+'''
+Simple parser for PTASL
+
+The use of globals causes a segmentation fault if running twice in iPython
+'''
+import re
+#import numpy as np
+
+# Defaults
+CONFIG_FILE = "config.dat"
+
+PI = 3.14159265359
+UNITS = "RADIANS"
+unitfunc = lambda x: x
+cameraunitfunc = lambda x: x*180/PI
+COLOR_PULSAR = (0.0, 0.0, 1.0)
+COLOR_BEAM = (1.0, 1.0, 0.0)
+COLOR_PULSE = (0.0, 1.0, 0.0)
+COLOR_EARTH = (0.0, 0.0, 1.0)
+COLOR_STAR = (1.0, 1.0, 1.0)
+BEAMZ = 2
+BEAMN = 20
+BEAMR = 1
+RADIUS_EARTH = 1.5
+RADIUS_PULSAR = 0.3
+
+BGCOLOR = (0.0, 0.0, 0.0)
+FGCOLOR = (0.0, 0.0, 0.0)
+N_FRAMES = 1
+RANDOM_SEED = 1
+N_STARS = 100
+SKYBOX = 100
+SPHERE_RESOLUTION = 101j
+SIZE = (600, 400)
+
+CAMERA_ZOOM = 3
+CAMERA_AZIMUTH = 0
+CAMERA_ELEVATION = 90
+CAMERA_ROTATE_AZIMUTH = 0
+CAMERA_ROTATE_ELEVATION = 0
+
+#To add to parser:
+OUTPUT_FILENAME_FORMAT = "frame%04d.png" #how to add this in?
+#Flag to see if you want to animate? = number of frames?
+
+
+
+PULSARS = []
+
+
+FILE = open(CONFIG_FILE,'r')
+lines = map(lambda x: x.strip(),FILE.readlines())
+FILE.close()
+
+
+for line in lines:    
+    if len(line) == 0 or line[0] == '#':
+        continue
+
+    m = re.match("PULSAR",line)
+    if m != None:
+        #Need to do things here
+        PULSARS.append(",".join(line.split()[1:]))
+
+        
+    
+    
+
+    else:
+        m = re.match(r"([a-zA-Z]+)\s([a-zA-Z]+)\s([a-zA-Z]+)",line)
+        if m == None:
+            m = re.match(r"([a-zA-Z]+)\s([a-zA-Z]+)",line)
+            if m == None:
+                m = re.match(r"([a-zA-Z]+)",line)
+                if m.group(1).upper()[0:4] == "BEAM": #BEAMZ, BEAMN, BEAMR
+                    L = m.group(1).upper()[4]
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require %s as one value"%L)
+                    exec("BEAM%s = float(value.group(0))"%L)
+                elif m.group(1).upper() == "FRAMES":
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require one integer value")
+                    exec("N_FRAMES = int(value.group(0))")
+                elif m.group(1).upper() == "SEED":
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require one integer value")
+                    exec("RANDOM_SEED = int(value.group(0))")
+                elif m.group(1).upper() == "STARS":
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require one integer value")
+                    exec("N_STARS = int(value.group(0))")
+                elif m.group(1).upper() == "SKYBOX":
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require one integer value")
+                    exec("SKYBOX = int(value.group(0))")
+                elif m.group(1).upper()[1:7] == "GCOLOR":
+                    rgb = re.findall("[0-9.]*[0-9]+",line)
+                    if len(rgb) != 3:
+                        raise IndexError("Require RGB as three values")
+                    rgb = tuple(map(float,rgb))
+                    exec("%sGCOLOR = %s"%(m.group(1).upper()[0],str(rgb)))
+                elif m.group(1).upper() == "SPHEREPTS":
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require one integer value")
+                    SPHERE_RESOLUTION = int(value.group(0))*1j
+                elif m.group(1).upper() == "SIZE":
+                    xy = re.findall("[0-9.]*[0-9]+",line)
+                    if len(xy) != 2:
+                        raise IndexError("Require XY as two values")
+                    xy = tuple(map(float,xy))
+                    SIZE = xy
+
+
+
+
+                else: #Arbitrarily define variables
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        continue
+                    exec("%s = float(value.group(0))"%m.group(1)) #use eval?
+
+
+            else:
+                if m.group(1).upper() == "COLOR":
+                    rgb = re.findall("[0-9.]*[0-9]+",line)
+                    if len(rgb) != 3:
+                        raise IndexError("Require RGB as three values")
+                    rgb = tuple(map(float,rgb))
+                    if m.group(2).upper() == "PULSAR":
+                        COLOR_PULSAR = rgb
+                    elif m.group(2).upper() == "BEAM":
+                        COLOR_BEAM = rgb
+                    elif m.group(2).upper() == "PULSE":
+                        COLOR_PULSE = rgb
+                    elif m.group(2).upper() == "EARTH":
+                        COLOR_EARTH = rgb
+                    elif m.group(2).upper() == "STAR":
+                        COLOR_STAR = rgb
+                elif m.group(1).upper() == "RADIUS":
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require one numerical value")
+                    if m.group(2).upper() == "EARTH":
+                        RADIUS_EARTH = float(value)
+                    elif m.group(2).upper() == "PULSAR":
+                        RADIUS_PULSAR = float(value)
+
+                elif m.group(1).upper() == "CAMERA":
+                    value = re.search("[0-9.]*[0-9]+",line)
+                    if value == None:
+                        raise IndexError("Require one numerical value")
+                    if m.group(2).upper() == "ZOOM":
+                        CAMERA_ZOOM = float(value.group(0))
+                    elif m.group(2).upper() == "AZIMUTH":
+                        CAMERA_AZIMUTH = cameraunitfunc(float(value.group(0)))
+                    elif m.group(2).upper() == "ELEVATION":
+                        CAMERA_ELEVATION = cameraunitfunc(float(value.group(0)))
+                    
+
+
+                elif m.group(1).upper() == "UNITS":
+                    if m.group(2).upper() == "DEGREES":
+                        UNITS = "DEGREES"
+                        unitfunc = lambda x: x*PI/180
+                        cameraunitfunc = lambda x: x
+                    elif m.group(2).upper() == "RADIANS":
+                        UNITS = "RADIANS"
+                        unitfunc = lambda x: x
+                        cameraunitfunc = lambda x: x*180/PI
+        else:
+            if m.group(1).upper() == "CAMERA":
+                value = re.search("[0-9.]*[0-9]+",line)
+                if value == None:
+                    raise IndexError("Require one numerical value")
+            
+                if m.group(2).upper() == "ROTATE" and m.group(3).upper() == "AZIMUTH":
+                    CAMERA_ROTATE_AZIMUTH = cameraunitfunc(float(value.group(0)))
+                elif m.group(2).upper() == "ROTATE" and m.group(3).upper() == "ELEVATION":
+                    CAMERA_ROTATE_ELEVATION = cameraunitfunc(float(value.group(0)))
---
+++ pta_animation.py
@@ -0,0 +1,260 @@
+#from mpl_toolkits.mplot3d import Axes3D
+#import mpl_toolkits.mplot3d.art3d as art3d
+#from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+#from matplotlib.pyplot import *
+#from matplotlib.patches import Polygon,Circle
+
+'''
+12/21/2014
+
+Speed up: make beam/cap there own classes and store x,y,z point matrices
+
+
+
+
+'''
+
+import numpy as np
+import subprocess
+from mayavi import mlab
+
+# Remove vtk warnings
+# https://github.com/enthought/mayavi/issues/3
+try:
+    __IPYTHON__
+    ipython = True
+except NameError:
+    ipython = False
+    ### THIS CAUSES ERRORS IN IPYTHON
+    import vtk
+    output=vtk.vtkFileOutputWindow()
+    output.SetFileName("log.txt")
+    vtk.vtkOutputWindow().SetInstance(output)
+
+
+
+from configuration import *
+
+
+
+sin = lambda x: np.sin(unitfunc(x))
+cos = lambda x: np.cos(unitfunc(x))
+
+
+
+def gaussian(x,a,b,c):
+    return a*np.exp(-0.5*((x-b)/c)**2)
+
+
+
+
+class Pulsar:
+    def __init__(self,xp,yp,zp,period,timestep=0.0,rotaxis=(0,0,0),beamZ=BEAMZ,xrot=0.0,yrot=0.0,zrot=0.0,pulsephase = 0.2):
+        # Pulsar characteristics
+        self.xp = xp
+        self.yp = yp
+        self.zp = zp
+        self.period = period
+        self.xrotaxis,self.yrotaxis,self.zrotaxis = rotaxis #could go to a two coordinate system?
+
+        self.phase = 0
+        # Initial conditions
+        self.xrot = xrot
+        self.yrot = yrot
+        self.zrot = zrot
+        self.pulsephase = pulsephase #more to do here, the initial pulse phase
+
+        # Generation
+        self.beam1,self.cap1 = self.plot_beam(beamZ,xrot,yrot,zrot)
+        self.beam2,self.cap2 = self.plot_beam(-1*beamZ,xrot,yrot,zrot)
+        self.sphere = self.plot_sphere()
+        self.pulsetrain = self.plot_pulsetrain()
+
+        # Animation 
+        self.timestep = timestep
+        self.Rmatrix = self.build_Rmatrix()
+
+    def plot_beam(self,Z=1,xrot=None,yrot=None,zrot=None): #modified from
+        n = BEAMN
+        t = np.linspace(-np.pi, np.pi, n)
+        z = np.exp(1j * t)
+        x = z.real.copy()*BEAMR
+        y = z.imag.copy()*BEAMR
+        z = np.zeros_like(x)
+
+        triangles = [(0, i, i + 1) for i in range(1, n)]
+        x = np.r_[0, x]
+        y = np.r_[0, y]
+        z = np.r_[Z, z] - Z
+        xcap = np.copy(x)
+        ycap = np.copy(y)
+        zcap = np.zeros_like(x)-Z
+        t = np.r_[0, t]
+
+
+        if xrot != 0.0 or yrot != 0.0 or zrot != 0.0:
+            pts = np.matrix(np.vstack((x,y,z)))
+            R = np.identity(3)
+            if xrot != 0.0:
+                c = cos(xrot)
+                s = sin(xrot)
+                R = np.matrix([[1,0,0],[0,c,-s],[0,s,c]])*R
+            if yrot != 0.0:
+                c = cos(yrot)
+                s = sin(yrot)
+                R = np.matrix([[c,0,s],[0,1,0],[-s,0,c]])*R
+            if zrot != 0.0:
+                c = cos(zrot)
+                s = sin(zrot)
+                R = np.matrix([[c,-s,0],[s,c,0],[0,0,1]])*R
+            
+            
+            for i in range(len(x)):
+                x[i],y[i],z[i] = R*pts[:,i]
+            ptscap = np.matrix(np.vstack((xcap,ycap,zcap)))
+            for i in range(len(x)):
+                xcap[i],ycap[i],zcap[i] = R*ptscap[:,i]
+        beam = mlab.triangular_mesh(x+self.xp, y+self.yp, z+self.zp, triangles,color=COLOR_BEAM)
+        #Plot cap
+        cap = mlab.triangular_mesh(xcap+self.xp,ycap+self.yp,zcap+self.zp,triangles,color=COLOR_BEAM)
+        return beam,cap
+
+    def plot_sphere(self):
+        # Create a sphere for the pulsar
+        phi, theta = np.mgrid[0:np.pi:SPHERE_RESOLUTION, 0:2 * np.pi:SPHERE_RESOLUTION]
+       
+        x = RADIUS_PULSAR * np.sin(phi) * np.cos(theta)
+        y = RADIUS_PULSAR * np.sin(phi) * np.sin(theta)
+        z = RADIUS_PULSAR * np.cos(phi)
+        sphere = mlab.mesh(x+self.xp , y+self.yp, z+self.zp,color=COLOR_PULSAR)
+        return sphere
+
+    def plot_pulsetrain(self):
+        self.pulsex = np.linspace(self.xp,0,100) #these do not change
+        self.pulsey = np.linspace(self.yp,0,100)
+        self.pulsez = np.linspace(self.zp,0,100)        
+        self.pulser = np.sqrt(self.pulsex**2 + self.pulsey**2 + self.pulsez**2)
+        self.Npulses = int(np.ceil(np.max(self.pulser)/(2*np.pi)))+1 #+1 for loop, move this there?
+        zpulse = np.zeros_like(self.pulsez)
+
+        for n in range(self.Npulses):
+            zpulse += gaussian(self.pulser,3.0,n*2*np.pi,0.3) #change pulse width        
+
+        pulsetrain = mlab.plot3d(self.pulsex,self.pulsey,self.pulsez+zpulse,color=COLOR_PULSE)
+        return pulsetrain
+
+
+    def get_components(self):
+        return self.sphere,self.beam1,self.cap1,self.beam2,self.cap2
+
+
+    def build_Rmatrix(self):
+        self.rotation = self.timestep * (2*np.pi/self.period)
+
+
+        R = np.matrix([[np.cos(self.rotation),-np.sin(self.rotation),0],[np.sin(self.rotation),np.cos(self.rotation),0],[0,0,1]])
+            
+        RXneg = np.matrix([[1,0,0],[0,cos(-self.xrotaxis),-sin(-self.xrotaxis)],[0,sin(-self.xrotaxis),cos(-self.xrotaxis)]])
+        RXpos = np.matrix([[1,0,0],[0,cos(self.xrotaxis),-sin(self.xrotaxis)],[0,sin(self.xrotaxis),cos(self.xrotaxis)]])
+            
+        RYneg = np.matrix([[cos(-self.yrotaxis),0,sin(-self.yrotaxis)],[0,1,0],[-sin(-self.yrotaxis),0,cos(-self.yrotaxis)]])
+        RYpos = np.matrix([[cos(self.yrotaxis),0,sin(self.yrotaxis)],[0,1,0],[-sin(self.yrotaxis),0,cos(self.yrotaxis)]])
+
+        RZneg = np.matrix([[cos(-self.zrotaxis),-sin(-self.zrotaxis),0],[sin(-self.zrotaxis),cos(-self.zrotaxis),0],[0,0,1]])
+        RZpos = np.matrix([[cos(self.zrotaxis),-sin(self.zrotaxis),0],[sin(self.zrotaxis),cos(self.zrotaxis),0],[0,0,1]])
+
+
+        R = RZpos*RYpos*RXpos*R*RXneg*RYneg*RZneg
+        return R
+
+    def rotate(self):
+        items = self.get_components()[1:]
+        
+        #print self.phase
+        for item in items:
+            x,y,z = item.mlab_source.x-self.xp,item.mlab_source.y-self.yp,item.mlab_source.z-self.zp
+
+            pts = np.matrix(np.vstack((x,y,z)))
+            for i in range(len(x)):
+                x[i],y[i],z[i] = self.Rmatrix*pts[:,i]
+            item.mlab_source.set(x=x+self.xp,y=y+self.yp,z=z+self.zp)
+
+    def propagate(self):
+        zpulse = np.zeros_like(self.pulsez)
+
+        self.phase += np.abs(self.rotation/(2*np.pi))
+        if self.phase >= 1.0:
+            self.phase = self.phase % 1
+
+
+        for n in range(self.Npulses):
+            zpulse += gaussian(self.pulser,3.0,(n-self.phase)*2*np.pi,0.3) #change pulse width
+        
+        self.pulsetrain.mlab_source.set(z=self.pulsez+zpulse)
+            
+    def animate(self):
+        self.rotate()
+        self.propagate()
+    
+
+
+
+mlab.figure(1, bgcolor=BGCOLOR, fgcolor=FGCOLOR, size=SIZE)
+
+#Make Earth
+
+phi, theta = np.mgrid[0:np.pi:SPHERE_RESOLUTION, 0:2 * np.pi:SPHERE_RESOLUTION]
+x = RADIUS_EARTH * np.sin(phi) * np.cos(theta)
+y = RADIUS_EARTH * np.sin(phi) * np.sin(theta)
+z = RADIUS_EARTH * np.cos(phi)
+earth = mlab.mesh(x, y, z,color=COLOR_EARTH)
+
+
+
+#Star field
+
+np.random.seed(RANDOM_SEED)
+x = np.random.uniform(-SKYBOX,SKYBOX,N_STARS)
+y = np.random.uniform(-SKYBOX,SKYBOX,N_STARS)
+z = np.random.uniform(-SKYBOX,SKYBOX,N_STARS)
+mlab.points3d(x,y,z,color=COLOR_STAR,scale_factor=0.5)
+
+
+
+ps = []
+for PULSAR in PULSARS:
+    ps.append(eval("Pulsar(%s)"%PULSAR))
+
+mlab.view(CAMERA_AZIMUTH,CAMERA_ELEVATION)
+f = mlab.gcf()
+f.scene.camera.zoom(CAMERA_ZOOM)
+
+#mlab.show()
+#raise SystemExit
+
+
+@mlab.animate(delay=50,ui=False)
+def anim():
+    i=0
+    while i < N_FRAMES:
+        f.scene.camera.azimuth(CAMERA_ROTATE_AZIMUTH)
+        f.scene.camera.elevation(CAMERA_ROTATE_ELEVATION)
+        for p in ps:
+            p.animate()
+        f.scene.render()
+        f.scene.save(OUTPUT_FILENAME_FORMAT%i)
+        i+=1
+        print "(%i/%i)"%(i,N_FRAMES)
+        yield
+    print "Done"
+    if not ipython:
+        subprocess.call("rm log.txt",shell=True)
+a = anim()
+
+mlab.show()
+
+#try:
+#    subprocess.call("convert -delay 5 *png -loop 0 animated.gif",shel=True)
+
+
+#convert -delay 5 *png -loop 0 animated.gif
